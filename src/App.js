1a93412c4cf367f084c6c6b08a91fc5cc00ca8f8
Pruek <arniwatt.c@ku.th>
2019-12-23 20:20:34 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import FlipPage from '../src/component/FlipPage/flippage'
+
+import './App.css'
+
+class App extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <FlipPage />
+      </React.Fragment>
+    )
+  }
+
+}
+
+export default App
---
+++ src/component/FlipPage/flippage.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import FlippingPages from 'flipping-pages'
+import 'flipping-pages/FlippingPages.css'
+
+import menu1 from '../../img/menu1.jpg';
+import menu2 from '../../img/menu2.jpg';
+import menu3 from '../../img/menu3.jpg';
+
+import './flippage.css'
+
+class FlipPage extends Component {
+
+    constructor(props) {
+        super(props)
+        this.totalPages = 4
+        this.state = {
+            selected: 0,
+        }
+        this.handleSelectedChange = this.handleSelectedChange.bind(this)
+        this.previous = this.previous.bind(this)
+        this.next = this.next.bind(this)
+    }
+
+    handleSelectedChange(selected) {
+        this.setState({ selected })
+    }
+
+    previous() {
+        this.setState(state => ({
+            selected: state.selected - 1
+        }))
+    }
+
+    next() {
+        this.setState(state => ({
+            selected: state.selected + 1
+        }))
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <FlippingPages
+                    className="App-pages"
+                    direction="horizontal"
+                    selected={this.state.selected}
+                    onSelectedChange={this.handleSelectedChange}
+                    touch-action="none"
+                >
+                    <img src={menu1} />
+                    <img src={menu2} />
+                    <img src={menu3} />
+                </FlippingPages>
+                <button
+                    onClick={this.previous}
+                    disabled={!this.state.selected}
+                >Previous</button>
+                <button
+                    onClick={this.next}
+                    disabled={this.state.selected + 1 === this.totalPages}
+                >Next</button>
+            </div>
+        )
+    }
+
+}
+
+export default FlipPage
